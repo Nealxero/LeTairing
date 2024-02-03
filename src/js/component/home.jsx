b788283baa262cfe3e3ed76a4e32ae76c2981bf4
Nealxero <santixero@gmail.com>
2024-02-03 15:21:19 +0000

    few changes and components added (Cards)

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Navbar } from "./navbar";
+import { CardimG } from "./card";
 //include images into your bundle
 // "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
+		<div className="text-center" id="mainBody">
 			<Navbar></Navbar>
 			<h1 className="text-center mt-5">LetTairing</h1>
-			<figure class="wp-block-image alignwide size-full is-style-rounded"><img src="https://s0.wp.com/wp-content/themes/pub/twentytwentyfour/assets/images/building-exterior.webp" alt="Building exterior in Toronto, Canada"/></figure>
+			<div>
+				<CardimG></CardimG>
+			</div>
+
 		</div>
 	);
 };
