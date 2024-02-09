216842257d74d5c08311608cbe84be56cf8ffac9
Nealxero <santixero@gmail.com>
2024-02-09 15:26:01 +0000

    Card carousel implemented, all images uploaded and styles added

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,22 +1,69 @@
-
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import "../../styles/card.css";
+import cafe from "../../img/cafe.jpg";
+import { useState } from "react";
 
-export const CardimG =  () => {
+export const Carousel = ({ cards }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? cards.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === cards.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
-    <Card style={{ width: '18rem', }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body id="CardB">
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div style={{ maxWidth: "400px", margin: "0 auto", position: "relative" }}>
+      {/* Adjust the max width and margin */}
+      <div style={{ position: "relative", display: "flex" }}>
+        <button
+          style={{
+            alignSelf: "center",
+            border: "none",
+            backgroundColor: "transparent",
+            fontSize: "20px",
+            padding: "5px",
+            cursor: "pointer",
+          }}
+          onClick={handlePrev}
+        >
+          {"<"}
+        </button>
+        <Card style={{ width: "100%" }}>
+          {/* Set the width to 100% */}
+          <Card.Img
+            variant="top"
+            src={cards[currentIndex].image}
+            style={{ height: "contain", objectFit: "contain" }}
+          />{" "}
+          {/* Adjust the height */}
+          <Card.Body>
+            <Card.Title>{cards[currentIndex].title}</Card.Title>
+            <Card.Text>{cards[currentIndex].content}</Card.Text>
+          </Card.Body>
+        </Card>
+        <button
+          style={{
+            alignSelf: "center",
+            border: "none",
+            backgroundColor: "transparent",
+            fontSize: "20px",
+            padding: "5px",
+            cursor: "pointer",
+          }}
+          onClick={handleNext}
+        >
+          {">"}
+        </button>
+      </div>
+    </div>
   );
-}
-
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Navbar } from "./navbar";
-import { CardimG } from "./card";
-//include images into your bundle
-// "../../img/rigo-baby.jpg";
 
+
+import cafe from "../../img/cafe.jpg";
+import desayuno from "../../img/desayuno.jpg"
+import { Carousel } from "./card";
 //create your first component
+
 const Home = () => {
+	
+	const cards = [
+		{ title: 'Card 1', content: 'Content for Card 1', image: cafe },
+		{title : 'Card 2', content : 'Content Card 2 ', image: desayuno}
+	  ];
+
 	return (
 		<div className="text-center" id="mainBody">
 			<Navbar></Navbar>
 			<h1 className="text-center mt-5">LetTairing</h1>
 			<div>
-				<CardimG></CardimG>
+				<Carousel cards={cards}/>
 			</div>
 
 		</div>
