ff0c72e78e6723efbde97883c7c7f66985ec0418
Nealxero <santixero@gmail.com>
2024-02-24 15:22:23 +0000

    Form created and functional (not final styles) Landing page created (not final style)

--- src/js/component/contact.jsx
+++ src/js/component/contact.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import "../../styles/contact.css";
 
 export const Contact = () => {
+    const form = useRef();
 
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+        .sendForm('service_dga9o5e', 'template_99wkc9v', form.current, {
+          publicKey: 'C9n5VExVOU7tlAzn6',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    };
 return (
     <div className="text-center">
         <h1> Contacta Conmigo </h1>
+        <form ref={form} onSubmit={sendEmail} className="simple-form">
+      <div className="form-group">
+        <label htmlFor="user_name">Nombre</label>
+        <input type="text" name="user_name" id="user_name" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="user_email">Email</label>
+        <input type="email" name="user_email" id="user_email" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Mensaje</label>
+        <textarea name="message" id="message"></textarea>
+      </div>
+      <div className="form-group">
+        <input type="submit" value="Send" />
+      </div>
+    </form>
     </div>
 )
 }
--- src/js/component/presentacion.jsx
+++ src/js/component/presentacion.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import cafe from "../../img/cafe.jpg";
+import "../../styles/inicio.css";
 
-export const Inicio = () => { 
-return (
-    <div className="text-center mt-5">
-        <h1>
-            Descubre el mundo del Lettering y atrae a las personas con diseños llamativos y personalizados, ideal para negocios abiertos de calle
-        </h1>
+export const Inicio = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6">
+          <img src={cafe} alt="Lettering" className="img-fluid" />
+        </div>
+        <div className="col-md-6 d-flex align-items-center justify-content-center">
+          <div className="text-center">
+            <h1 className="stylish-text">
+              Descubre el mundo del Lettering y atrae a las personas con diseños
+              llamativos y personalizados, ideal para negocios abiertos de calle
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
-)
-}
+  );
+};
